d825e322e38abd43ba9fc94fd4d61a6c4f425a56
winerlu <winerlu@tencent.com>
2022-04-15 00:36:45 +0800

    feat: add blogs

---
+++ mock/const.ts
@@ -0,0 +1,79 @@
+export const articleList = [
+  {
+    date: "2020年8月20日",
+    title: "vue3-tsx",
+    desc: "一个Vue3与TS的模版仓库",
+    type: "Git仓库",
+    href: "https://github.com/justwiner/vue3-tsx",
+  },
+  {
+    date: "2020年7月17日",
+    title: "Vue3全家桶",
+    desc: "Vite + TSX",
+    type: "博客",
+    href: "https://segmentfault.com/a/1190000023267394",
+  },
+  {
+    date: "2020年5月27日",
+    title: "axios重复请求缓存",
+    desc: "axios请求的缓存与重复请求过滤的封装",
+    type: "博客",
+    href: "https://segmentfault.com/a/1190000022758406",
+  },
+  {
+    date: "2019年8月2日",
+    title: "微前端",
+    desc: "微前端如何技术选型",
+    type: "博客",
+    href: "https://segmentfault.com/a/1190000019957130",
+  },
+  {
+    date: "2019年4月23日",
+    title: "microfront-end-single-spa",
+    desc: "基于single-spa的前端微服务架构",
+    type: "Git仓库",
+    href: "https://github.com/justwiner/microfront-end-single-spa",
+  },
+  {
+    date: "2018年5月20日",
+    title: "闭包",
+    desc: "如何理解闭包概念",
+    type: "博客",
+    href: "https://segmentfault.com/a/1190000014940837",
+  },
+  {
+    date: "2018年5月20日",
+    title: "Promise与async/await",
+    desc: "异步编程",
+    type: "博客",
+    href: "https://segmentfault.com/a/1190000014938839",
+  },
+  {
+    date: "2018年4月16日",
+    title: "HTTP协议",
+    desc: "理解HTTP协议",
+    type: "博客",
+    href: "https://segmentfault.com/a/1190000014413171",
+  },
+  {
+    date: "2018年4月16日",
+    title: "HTTP状态码",
+    desc: "",
+    type: "博客",
+    href: "https://segmentfault.com/a/1190000014406838",
+  },
+  {
+    date: "2018年4月16日",
+    title: "web性能优化",
+    desc: "前端性能优化的方法",
+    type: "博客",
+    href: "https://segmentfault.com/a/1190000014406838",
+  },
+  {},
+];
+
+export const userInfo = {
+  avator: "/avator.jpeg",
+  title: "Winer",
+  subTitle: "列出个人博客、代码仓库等，更高效的阅读",
+};
---
+++ pages/index.tsx
@@ -0,0 +1,52 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import { articleList, userInfo } from "../mock/const";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles["avator-container"]}>
+          <Image
+            className={styles.avator}
+            width={150}
+            height={150}
+            src={userInfo.avator}
+            alt="头像"
+          />
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles["title-container"]}>
+          <div className={styles.title}>{userInfo.title}</div>
+          <div className={styles.subTitle}>{userInfo.subTitle}</div>
+        </div>
+        <div className={styles["article-content"]}>
+          {articleList?.map((item, index) => (
+            <div key={index} className={styles["article-item"]}>
+              <div className={styles["article-item-date"]}>
+                <div className={styles["article-type"]}>{item.type}</div>
+                <div>{item.date}</div>
+              </div>
+              <div className={styles["article-item-info"]}>
+                <div className={styles["article-item-title"]}>
+                  {item.href ? (
+                    <a href={item.href} target="_blank" rel="noreferrer">
+                      {item.title}
+                    </a>
+                  ) : (
+                    item.title
+                  )}
+                </div>
+                <div className={styles["article-item-desc"]}>{item.desc}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
